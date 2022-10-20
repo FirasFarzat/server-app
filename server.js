8c321b595cc35a64dbc65f01982386e86c40b9d8
Firas Farzat <admin@syriancloud.net>
2022-10-20 22:41:54 +0200

    add env file

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const Port = process.env.Port || 3000;
+require('dotenv').config()
+const Port = process.env.Port ;
 server.use(middlewares);
 server.use(router);
 server.listen(Port, () => {
-  console.log("JSON Server is running");
+  console.log(`JSON Server is running ${Port}`);
 });
